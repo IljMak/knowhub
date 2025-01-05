c1212621f51b5db678ba0543223115d70034af11
IljMak <makarchukilj@gmail.com>
2025-01-05 14:01:29 +0100

    New

--- app.py
+++ app.py
@@ -1,108 +1,48 @@
-from flask import Flask, render_template, redirect, url_for, request, abort, flash
-from flask_bootstrap import Bootstrap
-import forms
+from flask import Flask, render_template, request, redirect, url_for, session
+from db import db, QuizQuestion, Reward
+import random
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///knowhub.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'supersecretkey'
 
-app.config.from_mapping(
-    SECRET_KEY = 'secret_key_just_for_dev_environment',
-    BOOTSTRAP_BOOTSWATCH_THEME = 'pulse'
-)
+db.init_app(app)
 
-from db import db, Todo, List, insert_sample  # (1.)
-
-bootstrap = Bootstrap(app)
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 @app.route('/')
-@app.route('/index')
-def index():
-    return redirect(url_for('todos'))
+def home():
+    return render_template('home.html')
 
-@app.route('/todos/', methods=['GET', 'POST'])
-def todos():
-    form = forms.CreateTodoForm()
-    if request.method == 'GET':
-        todos = db.session.execute(db.select(Todo).order_by(Todo.id)).scalars()  # !!
-        return render_template('todos.html', todos=todos, form=form)
-    else:  # request.method == 'POST'
-        if form.validate():
-            todo = Todo(description=form.description.data)  # !!
-            db.session.add(todo)  # !!
-            db.session.commit()  # !!
-            flash('Todo has been created.', 'success')
-        else:
-            flash('No todo creation: validation error.', 'warning')
-        return redirect(url_for('todos'))
+@app.route('/quiz/<module>', methods=['GET', 'POST'])
+def quiz(module):
+    if request.method == 'POST':
+        question_id = int(request.form['question_id'])
+        selected_answer = request.form['answer']
+        question = QuizQuestion.query.get(question_id)
+        if question.correct_answer == selected_answer:
+            session['points'] = session.get('points', 0) + 10
+            return redirect(url_for('quiz', module=module))
+    question = random.choice(QuizQuestion.query.filter_by(module=module).all())
+    return render_template('quiz.html', question=question)
 
-@app.route('/todos/<int:id>', methods=['GET', 'POST'])
-def todo(id):
-    todo = db.session.get(Todo, id)  # !!
-    form = forms.TodoForm(obj=todo)  # (2.)  # !!
-    if request.method == 'GET':
-        if todo:
-            if todo.lists: form.list_id.data = todo.lists[0].id  # (3.)  # !!
-            choices = db.session.execute(db.select(List).order_by(List.name)).scalars()  # !!
-            form.list_id.choices = [(0, 'List?')] + [(c.id, c.name) for c in choices]  # !!
-            return render_template('todo.html', form=form)
-        else:
-            abort(404)
-    else:  # request.method == 'POST'
-        if form.method.data == 'PATCH':
-            if form.validate():
-                form.populate_obj(todo)  # (4.)
-                todo.populate_lists([form.list_id.data])  # (5.)  # !!
-                db.session.add(todo)  # !!
-                db.session.commit()  # !!
-                flash('Todo has been updated.', 'success')
-            else:
-                flash('No todo update: validation error.', 'warning')
-            return redirect(url_for('todo', id=id))
-        elif form.method.data == 'DELETE':
-            db.session.delete(todo)  # !!
-            db.session.commit()  # !!
-            flash('Todo has been deleted.', 'success')
-            return redirect(url_for('todos'), 303)
-        else:
-            flash('Nothing happened.', 'info')
-            return redirect(url_for('todo', id=id))
+@app.route('/profile')
+def profile():
+    points = session.get('points', 0)
+    return render_template('profile.html', points=points)
 
-@app.route('/lists/')
-def lists():
-    lists = db.session.execute(db.select(List).order_by(List.name)).scalars()  # (6.)  # !!
-    return render_template('lists.html', lists=lists)
+@app.route('/rewards')
+def rewards():
+    points = session.get('points', 0)
+    available_rewards = Reward.query.filter(Reward.cost <= points).all()
+    return render_template('rewards.html', points=points, rewards=available_rewards)
 
-@app.route('/lists/<int:id>')
-def list(id):
-    list = db.session.get(List, id)  # !!
-    if list is not None:
-        return render_template('list.html', list=list)
-    else:
-        return redirect(url_for('lists'))
+@app.route('/coop')
+def coop():
+    return render_template('coop.html')
 
-@app.route('/insert/sample')
-def run_insert_sample():
-    insert_sample()
-    return 'Database flushed and populated with some sample data.'
-
-@app.errorhandler(404)
-def http_not_found(e):
-    return render_template('404.html'), 404
-
-@app.errorhandler(500)
-def http_internal_server_error(e):
-    return render_template('500.html'), 500
-
-@app.get('/faq/<css>')
-@app.get('/faq/', defaults={'css': 'default'})
-def faq(css):
-    return render_template('faq.html', css=css)
-
-@app.get('/ex/<int:id>')
-@app.get('/ex/', defaults={'id':1})
-def ex(id):
-    if id == 1:
-        return render_template('ex1.html')
-    elif id == 2:
-        return render_template('ex2.html')
-    else:
-        abort(404)
+if __name__ == '__main__':
+    app.run(debug=True)
